95c994456c09ff7cbf5b47c699dfec0d8e90ff97
HedaetShahriar <shahriahedaet@gmail.com>
2025-05-05 16:18:17 +0600

    Footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -7,7 +7,9 @@ const MainLayout = () => {
     return (
         <>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className='min-h-[calc(100vh-306px)]'>
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </>
     );
